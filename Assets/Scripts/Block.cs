11a2bd1b02654e02674e55813a47258c2974bf62
Michael Schappel <mikeschappel@gmail.com>
2015-06-09 15:51:35 -0500

    New scenes and started game mechanics

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour
+{
+	public void Drop ()
+	{
+
+	}
+}
---
+++ Assets/Scripts/ManageBlocks.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ManageBlocks : MonoBehaviour
+{
+	private Queue<Block> blocks;
+
+	void Start ()
+	{
+		blocks = new Queue<Block> ();
+		GenerateBlocks ();
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	private void GenerateBlocks ()
+	{
+
+	}
+
+	private void DropNextBlock ()
+	{
+		blocks.Dequeue ().Drop ();
+	}
+}
