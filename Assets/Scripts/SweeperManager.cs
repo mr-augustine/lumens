233ebc7ea3d0d1c48ae963b0fafc81460b21ee08
Augustine <pushbuttonrobotics@gmail.com>
2015-07-12 14:16:41 -0500

    Added sweep iteration counter and current/next turn toggles

--- Assets/Scripts/SweeperManager.cs
+++ Assets/Scripts/SweeperManager.cs
@@ -8,27 +8,67 @@ public class SweeperManager : MonoBehaviour
 	private Vector3 moveDirection;
 	private Vector3 startingPosition;
 
+	public enum Turn { Even, 
+						Odd };
+	private int iteration; //!< the Timeline's current sweep iteration
+	private Turn current;  //!< shortcut that represents the current iteration
+	private Turn next;	   //!< shortcut that represents the next iteration
+
 	void Start ()
 	{
 		Invoke ("Begin", 1);
 		moveDirection = Vector3.right;
 		startingPosition = transform.position;
+
+		iteration = 1;
+		current = Turn.Odd;
+		next = Turn.Even;
+		PrintIteration();
+		PrintPosition ();
 	}
 
 	void Update ()
 	{
 		if (!active)
 			return;
-		transform.Translate (moveDirection * .05f);
+		//transform.Translate (moveDirection * .05f);
+		//aah I slowed down the Timeline sweep rate to make the pacing
+		//less frantic
+		transform.Translate (moveDirection * .02f);
+		PrintPosition ();
 	}
 
 	void OnCollisionEnter (Collision col)
 	{
 		transform.position = startingPosition;
+		IncrementIteration();
+		PrintIteration ();
 	}
 
 	private void Begin ()
 	{
 		active = true;
+	}
+
+	private void IncrementIteration() {
+		iteration += 1;
+
+		next = current;
+		current = (iteration % 2 == 0 ? Turn.Even : Turn.Odd);
+	}
+
+	private void PrintIteration() {
+		Debug.Log ("Iteration #" + iteration + " started; Current Turn: " + current +
+		           "; Next Turn: " + next);
+	}
+
+	private void PrintPosition() {
+		Debug.Log ("3D Position.x: " + this.transform.position.x);
+
+		// aah We use an offset of 8 (i.e. numColumns / 2) to convert between
+		// x-position in 3D space and the corresponding grid column
+		// TODO remove the magic number 8
+		Debug.Log ("Grid Position.column: " + 
+		           (Mathf.FloorToInt(this.transform.position.x) + 8));
 	}
 }
