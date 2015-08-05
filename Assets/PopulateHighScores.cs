15ef5868562970cce7eb84bd1c0cec95da4e12cf
mikeschap <mikeschappel@gmail.com>
2015-08-05 14:42:55 -0500

    high scores being populated

---
+++ Assets/PopulateHighScores.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PopulateHighScores : MonoBehaviour
+{
+	[SerializeField]
+	private Text
+		scoreBox;
+	private Vector3 boxLoc;
+	private RectTransform rec;
+
+	void Start ()
+	{
+		rec = GetComponent<RectTransform> ();
+		boxLoc = transform.position;
+		Populate ();
+	}
+
+	private void Populate ()
+	{
+		foreach (string s in DatabaseHandler.PullHighScores ()) {
+			Text temp = (Text)Instantiate (scoreBox, new Vector3(711, boxLoc.y, 0), Quaternion.identity);
+			temp.transform.SetParent (transform);
+			temp.text = s;
+
+//			Vector3 tempVec = temp.GetComponent<RectTransform> ().
+//			tempVec.x = 0;
+//			temp.GetComponent<RectTransform> ().localPosition = tempVec;
+
+			boxLoc += new Vector3 (0, -64, 0);
+
+			rec.SetSizeWithCurrentAnchors (RectTransform.Axis.Vertical, rec.rect.height + 64);
+		}
+	}
+
+}
