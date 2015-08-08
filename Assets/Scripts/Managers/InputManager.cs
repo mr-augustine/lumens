aba8938fdf7f4f8ddd8327b82502dfadb387628f
Trivette <joshuatrivette@gmail.com>
2015-08-08 12:45:03 -0500

    Added gamepad back

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -52,33 +52,33 @@ public class InputManager : MonoBehaviour
 			currentBlock.RotateClockwise ();
 		}
 
-//		float horizontalInput = Input.GetAxis ("Horizontal");
-//		float verticalInput = Input.GetAxis ("Vertical");
-//
-//		// Fast Down
-//		if (verticalInput < 0) {
-//			currentBlock.MoveDown ();
-//		}
-//
-//		// Move Left
-//		if (horizontalInput < 0) {
-//			currentBlock.MoveLeft ();
-//		}
-//
-//		// Move Right
-//		if (horizontalInput > 0) {
-//			currentBlock.MoveRight ();
-//		}
-//
-//		// Rotate Left
-//		if (Input.GetButtonUp ("Fire1")) {
-//			currentBlock.RotateCounterClockwise ();
-//		}
-//
-//		// Rotate Right
-//		if (Input.GetButtonUp ("Fire2")) {
-//			currentBlock.RotateClockwise ();
-//		}
+		float horizontalInput = Input.GetAxis ("Horizontal");
+		float verticalInput = Input.GetAxis ("Vertical");
+
+		// Fast Down
+		if (verticalInput < 0) {
+			currentBlock.MoveDown ();
+		}
+
+		// Move Left
+		if (horizontalInput < 0) {
+			currentBlock.MoveLeft ();
+		}
+
+		// Move Right
+		if (horizontalInput > 0) {
+			currentBlock.MoveRight ();
+		}
+
+		// Rotate Left
+		if (Input.GetButtonUp ("Fire1")) {
+			currentBlock.RotateCounterClockwise ();
+		}
+
+		// Rotate Right
+		if (Input.GetButtonUp ("Fire2")) {
+			currentBlock.RotateClockwise ();
+		}
 	}
 
 	/// <summary>
